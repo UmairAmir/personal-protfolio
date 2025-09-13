91315d7590f1131185b9e0a4913d2e2e9c67d5d5
Umair <umair.amir19999@gmail.com>
2025-09-13 13:13:45 +0500

    Fixed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Umair Amir',
+  description: 'Personal Portfolio of Umair Amir',
 }
 
 export default function RootLayout({
